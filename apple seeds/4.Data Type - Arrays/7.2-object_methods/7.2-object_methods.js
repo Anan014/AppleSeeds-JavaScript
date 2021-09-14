8a1813cedea8aaa1b8d4ad62a993cd588c488525
Anan014 <ananamer95@gmail.com>
2021-09-14 13:25:36 +0300

    7.2-ex-done

--- apple seeds/4.Data Type - Arrays/7.2-object_methods/7.2-object_methods.js
+++ apple seeds/4.Data Type - Arrays/7.2-object_methods/7.2-object_methods.js
@@ -8,7 +8,7 @@ const myCountry = {
         return `${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
     },
     checkIsland() {
-        return myCountry.isIsland = (this.neighbours.length > 0 ? true : false);
+        return myCountry.isIsland = (this.neighbours.length > 0 ? false : true);
     }
 }
 console.log(myCountry.describe());
