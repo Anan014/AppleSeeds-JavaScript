50138b6f6e298a7c467928843faa65890a4f2f43
Anan014 <ananamer95@gmail.com>
2021-09-14 13:20:57 +0300

    7.2-ex-done

--- apple seeds/4.Data Type - Arrays/7.2-object_methods/7.2-object_methods.js
+++ apple seeds/4.Data Type - Arrays/7.2-object_methods/7.2-object_methods.js
@@ -3,9 +3,14 @@ const myCountry = {
     capital: 'Washington DC',
     language: 'English',
     population: 328.2,
-    neighbours: ['Canada', 'Mexico', 'Bahamas', 'Cuba', 'Russia'],
+    neighbours: ['Canada', 'Mexico'],
     describe() {
         return `${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+    },
+    checkIsland() {
+        return myCountry.isIsland = (this.neighbours.length > 0 ? true : false);
     }
 }
 console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
+console.log(myCountry);
