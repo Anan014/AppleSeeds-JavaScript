a6c8527f4e04b017054f42775a412e756fcf375e
Anan014 <ananamer95@gmail.com>
2021-09-14 10:52:59 +0300

    6.4-ex-done

--- apple seeds/4.Data Type - Arrays/6.4-jumpers/6.4-jumpers.js
+++ apple seeds/4.Data Type - Arrays/6.4-jumpers/6.4-jumpers.js
@@ -1,17 +1,22 @@
 //1
-const jumps = [5, 5, 8, 7, -1, 5];
+const jumps = [5, 5, 8, 7, -1, 5, 10, 5, 15];
 
 //2
 console.log("2.");
 
 function avgJumper(jumps) {
-    let jumpers = new Array(jumps.length / 3).fill(0);
-    for (let i = 0; i < jumps.length / 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (jumps[3 * i + j] >= 0)
-                jumpers[i] += jumps[3 * i + j]
+    let jumpsForJumpers = 3;
+    let numOfValidJumps = 0;
+    let jumpers = new Array(jumps.length / jumpsForJumpers).fill(0);
+    for (let i = 0; i < jumps.length / jumpsForJumpers; i++) {
+        for (let j = 0; j < jumpsForJumpers; j++) {
+            if (jumps[jumpsForJumpers * i + j] >= 0) {
+                jumpers[i] += jumps[jumpsForJumpers * i + j]
+                numOfValidJumps++;
+            }
         }
-        jumpers[i] = jumpers[i] / 3;
+        jumpers[i] = jumpers[i] / numOfValidJumps;
+        numOfValidJumps = 0;
     }
     return jumpers;
 
