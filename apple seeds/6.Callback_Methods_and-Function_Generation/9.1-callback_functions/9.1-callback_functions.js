0594d3f37ac8f0ce92ff050b80dcbd0430ec2ca0
Anan014 <ananamer95@gmail.com>
2021-09-21 17:44:09 +0300

    9.1-ex-done

--- apple seeds/6.Callback_Methods_and-Function_Generation/9.1-callback_functions/9.1-callback_functions.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, -5, 666, 2, 400, 11];
-const desAsc = numbers.slice().sort((a, b) => a - b);
-const ascDes = numbers.slice().sort((a, b) => b - a);
-console.log(desAsc);
-console.log(ascDes);
