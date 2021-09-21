317e7662428121e50c39ada9df04ce2c4956a051
Anan014 <ananamer95@gmail.com>
2021-09-21 19:27:01 +0300

    9.1-ex-done

--- apple seeds/6.Callback_Methods_and-Function_Generation/9.1-callback_functions/9.1-callback_functions.js
+++ apple seeds/6.Callback_Methods_and-Function_Generation/9.1-callback_functions/9.1-callback_functions.js
@@ -1,3 +1,4 @@
+//1 ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 function isString(str, func) {
     if (typeof str === 'string' || str instanceof String)
         return func(str);
@@ -11,3 +12,39 @@ function checkString(str) {
 
 isString("Anan", checkString);
 isString(5, checkString);
+
+
+//2 ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+function firstWordUpperCase(str, func) {
+    const str1 = str.split(' ').map(word => word == str.split(' ')[0] ? word.toUpperCase() : word);
+    return func(str1)
+}
+
+function dashWords(str) {
+    console.log(str.join('-'));
+}
+
+firstWordUpperCase("Anan is ok thanks", dashWords);
+
+
+//3 ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+function emailCreator(str) {
+    const emailEnding = "@gmail.com";
+    newEmail = str.join('').concat(emailEnding);
+    console.log(newEmail);
+}
+
+firstWordUpperCase("julis", emailCreator);
+
+
+//4 ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+function multiFunc(arr, func) {
+    console.log(arr);
+    return func(arr.map(x => x * 2));
+}
+
+function printIt(arr) {
+    console.log(arr);
+}
+
+multiFunc([1, 2, 5, 8, 6, 4, 3, 28], printIt);
