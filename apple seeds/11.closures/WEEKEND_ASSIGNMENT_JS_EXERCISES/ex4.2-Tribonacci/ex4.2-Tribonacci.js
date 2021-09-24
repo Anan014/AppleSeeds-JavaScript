199d9ee4f3be02f295f86a4a5df0331ea3fd6eac
Anan014 <ananamer95@gmail.com>
2021-09-24 14:02:41 +0300

    WE_AS_JS_EX-ex4.2-done

---
+++ apple seeds/11.closures/WEEKEND_ASSIGNMENT_JS_EXERCISES/ex4.2-Tribonacci/ex4.2-Tribonacci.js
@@ -0,0 +1,16 @@
+function fibonacci(signature, num) {
+    let sum = 0;
+    let arr = [];
+    arr[0] = signature[0];
+    arr[1] = signature[1];
+    arr[2] = signature[2];
+    for (let i = 3; i < num; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+    }
+    return arr;
+}
+const signature0 = [0, 0, 1];
+const signature1 = [1, 1, 1];
+
+console.log(fibonacci(signature0, 12));
+console.log(fibonacci(signature1, 12));
