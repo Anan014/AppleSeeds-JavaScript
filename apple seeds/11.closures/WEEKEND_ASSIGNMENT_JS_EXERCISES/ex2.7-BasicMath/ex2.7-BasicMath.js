a38ea1d6911f6c1111e844e5d2c286fbf12245fb
Anan014 <ananamer95@gmail.com>
2021-09-24 12:53:38 +0300

    WE_AS_JS_EX-ex2.7-done

---
+++ apple seeds/11.closures/WEEKEND_ASSIGNMENT_JS_EXERCISES/ex2.7-BasicMath/ex2.7-BasicMath.js
@@ -0,0 +1,25 @@
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+            break;
+        case '-':
+            return value1 - value2;
+            break;
+        case '*':
+            return value1 * value2;
+            break;
+        case '/':
+            return value1 / value2;
+            break;
+
+        default:
+            return "Enter valid operator!"
+            break;
+    }
+}
+
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5));
+console.log(basicOp('/', 49, 7));
