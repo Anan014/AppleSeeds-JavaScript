22b7e5f8026cd9287f12605c8b9e009b67780101
Anan014 <ananamer95@gmail.com>
2021-09-25 15:35:03 +0300

    WE_AS_JS_EX-ex6.3-done

---
+++ apple seeds/11.closures/WEEKEND_ASSIGNMENT_JS_EXERCISES/ex6.3-OrganizeStrings/ex6.3-OrganizeStrings.js
@@ -0,0 +1,12 @@
+function organizeStrings(str1, str2) {
+    let mySet = new Set;
+    let arr = [];
+    str1.split("").forEach(element => { mySet.add(element); });
+    str2.split("").forEach(element => { mySet.add(element); });
+
+    return Array.from(mySet).sort().join("");
+}
+
+const a = "xyaabbbccccdefww";
+const b = "xxxxyyyyabklmopq";
+console.log(organizeStrings(a, b));
