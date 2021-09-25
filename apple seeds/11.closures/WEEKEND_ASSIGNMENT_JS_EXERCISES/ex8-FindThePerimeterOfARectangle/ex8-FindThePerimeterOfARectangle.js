c4f7ed5112338c6b0b9b50224b0c6e00d42b99b1
Anan014 <ananamer95@gmail.com>
2021-09-25 16:53:36 +0300

    WE_AS_JS_EX-ex8-done

---
+++ apple seeds/11.closures/WEEKEND_ASSIGNMENT_JS_EXERCISES/ex8-FindThePerimeterOfARectangle/ex8-FindThePerimeterOfARectangle.js
@@ -0,0 +1,7 @@
+function findPerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
