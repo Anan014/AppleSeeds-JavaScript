8e5fea6c2a1fe4e9913d0de1f4b77679d762d288
Anan014 <ananamer95@gmail.com>
2021-09-26 12:50:05 +0300

    17.3-ex-done

---
+++ apple seeds/12.Elemenet-selectors-methods-classes/18.3-selecting_all_elements/script.js
@@ -0,0 +1,4 @@
+const myFirstDiv = document.querySelector("#myFirstDiv"),
+    mySecondDiv = document.querySelector("#mySecondDiv"),
+    myThirdDiv = document.querySelector("#myThirdDiv"),
+    myFourthDiv = document.querySelector("#myFourthDiv");
