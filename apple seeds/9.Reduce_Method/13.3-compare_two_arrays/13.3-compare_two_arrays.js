5adbcc93ec1a040d82272565aa1e75f5336ca47e
Anan014 <ananamer95@gmail.com>
2021-10-01 12:28:08 +0300

    13.-ex-done

---
+++ apple seeds/9.Reduce_Method/13.3-compare_two_arrays/13.3-compare_two_arrays.js
@@ -0,0 +1,26 @@
+const food = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber", "Olives"];
+const food1 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
+
+function comparFunction(arr1, arr2) {
+    let i = 0,
+        j = 0,
+        flag = 0;
+
+    while (i < arr1.length) {
+        while (j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+
+                console.log(arr1[i]);
+                flag++;
+            }
+            j++;
+        }
+        i++;
+        j = 0;
+    }
+    if (flag == 0) {
+        console.log(false);
+    }
+}
+
+comparFunction(food, food1);
