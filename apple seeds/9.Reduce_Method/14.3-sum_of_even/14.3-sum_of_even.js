f342de9761e2df9717e78ffb2b395508889aec3e
Anan014 <ananamer95@gmail.com>
2021-10-02 22:20:00 +0300

    14.3-ex-done

---
+++ apple seeds/9.Reduce_Method/14.3-sum_of_even/14.3-sum_of_even.js
@@ -0,0 +1,4 @@
+function getSumOfEven(arr) {
+    return arr[1] + arr[3] + arr[5] + arr[7] + arr[9]; //1) arr[arr.length] =NaN  2) c.log+vs debugger
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
