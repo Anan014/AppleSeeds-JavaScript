444d2597eb1f4beef8e620656e0576a2632c3046
Anan014 <ananamer95@gmail.com>
2021-10-03 20:58:04 +0300

    21.3-ex-done

--- apple seeds/14.Event_listeners/21.3-forEachDom/21.3-forEachDom.js
+++ apple seeds/14.Event_listeners/21.3-forEachDom/21.3-forEachDom.js
@@ -19,19 +19,14 @@ const users = [{
 ];
 
 const ol = document.createElement("ol");
-const li1 = document.createElement("li");
-const li2 = document.createElement("li");
-const li3 = document.createElement("li");
-li1.appendChild(document.createTextNode("Jimmy Arnold"));
-li1.setAttribute("data-id", "167464");
-ol.appendChild(li1);
 
-li2.appendChild(document.createTextNode("Martha Goldman"));
-li2.setAttribute("data-id", "578447");
-ol.appendChild(li2);
 
-li3.appendChild(document.createTextNode("Tim Burton"));
-li3.setAttribute("data-id", "864578");
-ol.appendChild(li3);
+users.forEach(element => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${element.firstName} ${element.lastName}`));
+    li.setAttribute("data-id", `${element.id}`);
+    ol.appendChild(li);
+});
+
 ol.style.listStyle = ("none");
 document.body.appendChild(ol);
