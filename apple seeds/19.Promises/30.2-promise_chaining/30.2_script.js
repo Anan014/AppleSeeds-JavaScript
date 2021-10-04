65b5aea579846b661d1db72b5ea475b1bb7c4b6e
Anan014 <ananamer95@gmail.com>
2021-10-04 14:08:34 +0300

    30.2-ex-done

--- apple seeds/19.Promises/30.2-promise_chaining/30.2_script.js
+++ apple seeds/19.Promises/30.2-promise_chaining/30.2_script.js
@@ -1,21 +1,39 @@
 const makeAllCaps = (arr) => {
-
     return new Promise((resolve, reject) => {
-
         setTimeout(() => {
-            let arrSplit = arr.split(" ").join("").split("");
-            arrSplit.forEach(element => {
-
+            let strings = arr.map(element => {
+                return (typeof element === 'string') ? element.toUpperCase() : reject("Error");
             });
-            console.log(arrSplit);
-        }, 1);
-
+            resolve(strings);
+        }, 1000);
     });
-
 }
 
 const sortWords = (arr) => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let strings = arr.map(element => {
+                return (typeof element === 'string') ? element : reject("Error 2");
+            });
+            resolve(strings.sort());
+        }, 1000);
+    });
 }
 
-makeAllCaps("Cillum eu quis nulla eiusmod commodo ullamco irure proident ad")
+const array = ["cillum", "eu", "quis", "nulla", "anan"];
+
+makeAllCaps(array)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log("first", error);
+    });
+
+sortWords(array)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
