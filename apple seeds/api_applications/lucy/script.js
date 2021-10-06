0eba592460b222d9aef36937531003490d1dbd32
Anan014 <ananamer95@gmail.com>
2021-10-06 14:48:49 +0300

    api-3.0-lucifer

--- apple seeds/api_applications/lucy/script.js
+++ apple seeds/api_applications/lucy/script.js
@@ -1,12 +1,19 @@
-const quote_box = document.querySelector(".quote__box");
+const quote_box = document.querySelector(".quote__box"),
+    quote_img = document.querySelector(".quote__img"),
+    quote_author = document.querySelector(".quote__author"),
+    quote_text = document.querySelector(".quote__text"),
+    btn = document.querySelector(".btn");
 
 async function getData() {
-    const quotes_data = await fetch('https://raw.githubusercontent.com/Anan014/AppleSeeds-JavaScript/main/apple%20seeds/api_applications/lucy/package.json');
+    const quotes_data = await fetch('https://raw.githubusercontent.com/Anan014/AppleSeeds-JavaScript/main/apple%20seeds/api_applications/lucy/allQuotes.json');
     const quotes = await quotes_data.json();
-    console.log(quotes);
-    console.log(quotes_data);
-    console.log("hello");
-    console.log(quotes[0]);
+
+    let randomQuoteNumber = Math.floor(Math.random() * quotes.length);
+
+    quote_text.innerHTML = `“ ${quotes[randomQuoteNumber].quote} ”`;
+    quote_author.innerHTML = `- ${quotes[randomQuoteNumber].author}`;
 }
 
-getData();
+btn.addEventListener('click', () => {
+    getData();
+});
