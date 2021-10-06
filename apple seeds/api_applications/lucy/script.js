944cc57a951a18a8e5b77e4d9d94b32f4afe4b06
Anan014 <ananamer95@gmail.com>
2021-10-06 13:39:46 +0300

    api-3.0-lucifer

--- apple seeds/api_applications/lucy/script.js
+++ apple seeds/api_applications/lucy/script.js
@@ -1,1 +1,11 @@
-const first_div = document.querySelector("first_div")
+const quote_box = document.querySelector(".quote__box");
+
+async function getData() {
+    const quotes_data = await fetch('https://raw.githubusercontent.com/Anan014/AppleSeeds-JavaScript/main/apple%20seeds/api_applications/lucy/package.json');
+    const quotes = await quotes_data.json();
+    console.log(quotes);
+    console.log(quotes_data);
+    console.log("hello");
+}
+
+getData();
